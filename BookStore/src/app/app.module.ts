44c24b9f8bed5cb71d057d1aee98da0949db4595
orItach <or_ith@walla.com>
2018-08-01 21:16:36 +0300

    Auth servicee and Alert service added

--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -8,22 +8,26 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AngularFontAwesomeModule } from "angular-font-awesome";
 import { MatButtonModule } from "@angular/material";
 import { MatFormFieldModule } from "@angular/material";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { LoginNavBarComponent } from "./navbar/login-nav-bar/login-nav-bar.component";
+import { LoginNavBarComponent } from './navbar/login-nav-bar/login-nav-bar.component';
 import { LogedNavBarComponent } from './navbar/loged-nav-bar/loged-nav-bar.component';
-import { NavBarComponent } from "./navbar/nav-bar/nav-bar.component";
+import { NavBarComponent } from './navbar/nav-bar/nav-bar.component';
 import { SmallBookViewComponent } from './shopping/small-book-view/small-book-view.component';
 import { CatalogComponent } from './shopping/catalog/catalog.component';
 import { GroupDiscussionComponent } from './groups/group-discussion/group-discussion.component';
 import { EnterGroupComponent } from './groups/enter-group/enter-group.component';
 import { BlogsComponent } from './groups/blogs/blogs.component';
-
+import { MainImageComponent } from './main-image/main-image.component';
+import { LoginComponent } from './login/login.component';
+import { AuthenticationService } from './services/authentication-service.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AlertService } from './services/alert-service.service';
 
 const routes: Routes = [
   {path: 'loginNav', component: LoginNavBarComponent},
   {path: 'logedNav/:manager', component: LogedNavBarComponent}
-]
+];
 
 @NgModule({
   declarations: [
@@ -35,7 +39,9 @@ const routes: Routes = [
     CatalogComponent,
     GroupDiscussionComponent,
     EnterGroupComponent,
-    BlogsComponent
+    BlogsComponent,
+    MainImageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -44,9 +50,14 @@ const routes: Routes = [
     MatButtonModule,
     MatFormFieldModule,
     NgbModule.forRoot(),
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
-  providers: [NgbModal],
+  providers: [
+    NgbModal,
+    AuthenticationService,
+    AlertService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
