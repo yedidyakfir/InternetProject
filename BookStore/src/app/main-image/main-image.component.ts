dda9bd9b4f4280dc41a2e85b88baa9650f207654
orItach <or_ith@walla.com>
2018-08-01 21:32:50 +0300

    main image commponent added

---
+++ BookStore/src/app/main-image/main-image.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-image',
+  templateUrl: './main-image.component.html',
+  styleUrls: ['./main-image.component.css']
+})
+
+export class MainImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
