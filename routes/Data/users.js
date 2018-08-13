8c957c2094de75764d78f1409a43ea0a6a47039a
orItach <or_ith@walla.com>
2018-08-13 15:31:53 +0300

    fix bad file committed

--- routes/Data/users.js
+++ routes/Data/users.js
@@ -64,8 +64,8 @@ router.get('/list',async function (req, res) {
 });
 
 router.post('/disable', async function(req,res) {
-    User.DISABLE(req.body.email);
-    res.json(true);
+    //User.DISABLE(req.body.email);
+    //res.json(true);
    if(req.isAuthenticated() && req.user.admin) {
        User.DISABLE(req.body.email);
        res.json(true);
@@ -74,8 +74,8 @@ router.post('/disable', async function(req,res) {
 });
 
 router.post('update',function (req,res) {
-    User.UPDATE(req.body.email,req.body.newUser);
-    res.json(true);
+    //User.UPDATE(req.body.email,req.body.newUser);
+    //res.json(true);
     if(req.isAuthenticated() && req.user.admin) {
         User.UPDATE(req.body.email,req.body.newUser);
         res.json(true);
