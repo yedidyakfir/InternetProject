34e70b529f003d067cfe8dcf4ddeac67e35d13ec
orItach <or_ith@walla.com>
2018-08-16 00:15:46 +0300

    basic paypal componenet

---
+++ BookStore/src/app/paypal/paypal.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-paypal';
+
+@Component({
+  selector: 'app-paypal',
+  templateUrl: './paypal.component.html',
+  styleUrls: ['./paypal.component.css']
+})
+export class Paypal implements OnInit {
+
+  public payPalConfig?: PayPalConfig;
+
+  ngOnInit(): void {
+    this.initConfig();
+  }
+
+  private initConfig(): void {
+    this.payPalConfig = new PayPalConfig(PayPalIntegrationType.ClientSideREST, PayPalEnvironment.Sandbox, {
+      commit: true,
+      client: {
+		  sandbox: 'AavkgQ4UPe7gPbjZK2Rb7FVnh79dnGHSD0dnMG70-jSNl_XJE8CNDDj8AgmtMLvx3YZ-A8jHTT-AhIfn'
+        //sandbox: 'yourSandboxKey'
+      },
+      button: {
+        label: 'paypal',
+      },
+      onPaymentComplete: (data, actions) => {
+        console.log('OnPaymentComplete');
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel');
+      },
+      onError: (err) => {
+        console.log('OnError');
+      },
+      transactions: [{
+        amount: {
+          currency: 'USD',
+          total: 9
+        }
+      }]
+    });
+  }
+}
