06d3ce5923c7d1922c06b7626eddff2af177758a
orItach <or_ith@walla.com>
2018-08-16 21:09:52 +0300

    fix main image, some refactor at register component

--- BookStore/src/app/NavBar/register/register.component.ts
+++ BookStore/src/app/NavBar/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../../services/UserService/user.service";
+import {User} from "../../../model/user";
 
 @Component({
   selector: 'app-register',
@@ -8,22 +9,23 @@ import { UserService } from "../../../services/UserService/user.service";
 })
 export class RegisterComponent implements OnInit {
 
-  email: string = '';
-  password: string = '';
+  user: User =new User();
   rePassword : string = '';
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.user.password="";
+    this.user.email="";
   }
 
   register() {
     console.log("get to register function");
-    if (this.password != this.rePassword)
+    if (this.user.password != this.rePassword)
     {
       alert("password are not the same");
       return;
     }
-    this.userService.register(this.email,this.password)
+    this.userService.register(this.user.email,this.user.password)
       .subscribe(res => {
         if(res == false)
           alert("something get wrong...");
