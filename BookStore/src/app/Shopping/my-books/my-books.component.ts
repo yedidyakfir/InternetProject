c2328f2ceb1ae1fcad773ce0a249e5b5f5d86ded
orItach <or_ith@walla.com>
2018-08-19 22:26:28 +0300

    basic my book component, need review and update

--- BookStore/src/app/Shopping/my-books/my-books.component.ts
+++ BookStore/src/app/Shopping/my-books/my-books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from "../../../model/book";
+import {BookService} from "../../../services/BookService/book.service";
 
 @Component({
   selector: 'app-my-books',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyBooksComponent implements OnInit {
 
-  constructor() { }
+  private booksInPage: number = 9;
+  private pageNumber:number = 0;
+
+  public books:Book[] = [];
+  constructor(public bookService: BookService) { }
 
   ngOnInit() {
+    this.bookService.getBookList()
+      .subscribe(bookRes => {
+        this.books = bookRes;
+        console.log(bookRes);
+      });
   }
 
+  public addBook(book:Book) {
+    this.books.push(book);
+    console.log(this.books);
+  }
+
+  public getBooks() {
+    this.bookService.getBookList(this.pageNumber * this.booksInPage, this.booksInPage)
+      .subscribe(bookRes => this.books = bookRes);
+    console.log(this.books);
+  }
+
+  public nextPage() {
+    console.log("nexy");
+    this.pageNumber += 1;
+    this.getBooks();
+  }
+
+  public previousPage() {
+    if(this.pageNumber > 0)
+      this.pageNumber -= 1;
+    this.getBooks();
+  }
 }
